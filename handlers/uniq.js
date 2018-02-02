99cef2457fef3c96914aed09d2ac20df21e14062
Алексей <oleksii.chepugov@ria.com>
2018-02-02 15:25:52 +0200

    uniq string

---
+++ handlers/uniq.js
@@ -0,0 +1,7 @@
+/**
+ * Creates uniq string
+ * @param {string} prefix - prefix for returned value
+ * @param {string} postfix - postfix for returned value
+ * @returns {string} - откорректированная позиция в массиве
+ */
+export default (prefix = '', postfix = '') => `${prefix}${Date.now()}${Math.random()}${postfix}`
