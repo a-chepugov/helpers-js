01f4be478c862d1e8770a126a9b5e4fb86d20a6a
Алексей <oleksii.chepugov@ria.com>
2018-02-02 16:29:59 +0200

    Function

--- Standard/index.js
+++ Standard/index.js
@@ -1,5 +1,6 @@
 export Object from './Object';
 export Promise from './Promise';
+export Function from './Function';
 export String from './String';
 export Number from './Number';
 export HTML from './HTML';
