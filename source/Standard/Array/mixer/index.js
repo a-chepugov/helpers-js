98cb5d14c1629a4030a823d17cd931562871cd32
a.chepugov <oleksii.chepugov@ria.com>
2018-07-24 19:22:53 +0300

    fix mixer with 0 portions

--- source/Standard/Array/mixer/index.js
+++ source/Standard/Array/mixer/index.js
@@ -1,18 +1,27 @@
 module.exports = function (arrays, portions) {
 	const generator = function* (arrays, portions) {
-		const iterators = Array.from(arrays, (item) => item[Symbol.iterator]());
 		portions = Array.from(portions, (item) => Number.isFinite(item) && item >= 0 ? item : 0);
-		while (iterators.length) {
-			for (let i in iterators) {
-				const it = iterators[i];
-				let p = portions[i];
+		let iterators = Array.from(arrays, (item) => item[Symbol.iterator]());
+
+		while (portions.some(((item) => item))) {
+			// убираем массивы, которые не нужно проходить
+			({iterators, portions} = portions.reduce((result, item, index) => {
+				if (item > 0) {
+					result.iterators.push(iterators[index]);
+					result.portions.push(item);
+				}
+				return result;
+			}, {iterators: [], portions: []}));
+
+			for (let index in iterators) {
+				const iterator = iterators[index];
+				let p = portions[index];
 				while (p > 0) {
 					p--;
-					let {value, done} = it.next();
+					let {value, done} = iterator.next();
 
 					if (done) {
-						iterators.splice(i, 1);
-						portions.splice(i, 1);
+						portions[index] = 0;
 					} else {
 						yield value;
 					}
