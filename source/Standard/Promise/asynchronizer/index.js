e26dfb1ccf739c86f4ce441d1cc746042cb186da
a.chepugov <oleksii.chepugov@ria.com>
2018-08-10 16:30:23 +0300

    doc asynchronizer

--- source/Standard/Promise/asynchronizer/index.js
+++ source/Standard/Promise/asynchronizer/index.js
@@ -1,3 +1,10 @@
+/**
+ * Call function in async manner
+ * @param {function} fn
+ * @param {(undefined|array)[]} args - array of arguments for fn invoke like fn.apply(thisArgs, args[x])
+ * @param {any} thisArgs - context for `fn`
+ * @return {Promise<any>}
+ */
 module.exports = (fn, args, thisArgs) =>
 	new Promise((resolve, reject) => {
 		try {
