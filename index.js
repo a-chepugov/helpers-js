d89ebcb0f9694a71d7c1d81afca7e9a21f55cfd5
a.chepugov <oleksii.chepugov@ria.com>
2018-08-13 17:15:35 +0300

    fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = {
-	Constants: require('source/Constants'),
-	HTML: require('source/HTML'),
-	Node: require('source/Node'),
-	Patterns: require('source/Patterns'),
-	Standard: require('source/Standard'),
+	Constants: require('./source/Constants'),
+	HTML: require('./source/HTML'),
+	Node: require('./source/Node'),
+	Patterns: require('./source/Patterns'),
+	Standard: require('./source/Standard'),
 };
