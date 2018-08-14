0ffb04539aa3edd4f830966478f33503ab66231a
a.chepugov <oleksii.chepugov@ria.com>
2018-08-14 12:15:34 +0300

    test random

---
+++ source/Standard/Number/random/test.js
@@ -0,0 +1,14 @@
+const expect = require('chai').expect;
+const tested = require('./index');
+
+describe('random', async function () {
+
+	describe('run', async function () {
+		it('is', async function () {
+
+			let r = tested(1000, 2000);
+			expect(r >= 1000 && r <= 2000).to.equal(true);
+		});
+	});
+
+});
