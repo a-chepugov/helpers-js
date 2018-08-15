1fbe2f3627e8526524589cbf8cf2dbcece58c628
a.chepugov <oleksii.chepugov@ria.com>
2018-08-15 17:29:07 +0300

    test random

--- source/Standard/Number/random/test.js
+++ source/Standard/Number/random/test.js
@@ -4,11 +4,13 @@ const tested = require('./index');
 describe('random', async function () {
 
 	describe('run', async function () {
-		it('is', async function () {
+
+		it('is in range', async function () {
 
 			let r = tested(1000, 2000);
 			expect(r >= 1000 && r <= 2000).to.equal(true);
 		});
+
 	});
 
 });
