9365ddd0526b447612583d5d8b33440d57a25a61
a.chepugov <oleksii.chepugov@ria.com>
2018-10-05 14:57:15 +0300

    new async-get

--- source/Standard/Object/async-get/index.js
+++ source/Standard/Object/async-get/index.js
@@ -4,64 +4,55 @@
  * Создает обертку для асинхронной инициализации
  * @name async-get
  * @memberof Standard/Object
- * @param {Object<Function>} source - словарь, элементами которого являются функции
- * @param {Number} timeout - ключ, который берется для создания нового словаря
- * @param {any} thisArg - контекст для вызова функций исходного словаря
- * @return {object}
+ * @param {Function} initiator - функция для инициализации. Получает перечень запрошенных полей
+ * @param {Object<Function>} formatter - словарь функций для постобработки результата
+ * @param {Number} timeout - задержка, для агрегации ключей
+ * @param {any} thisArg - контекст для вызова `initiator` и элементов formatter
+ * @return {Proxy}
  */
-module.exports = (source, after = {}, timeout, thisArg) => {
-	timeout = Number.isFinite(timeout) && timeout > 0 ? Math.floor(timeout) : 7;
-	const handlers = new Map();
-	const promises = new Map();
+module.exports = (initiator = new Function(), formatter = {}, timeout, thisArg = formatter) => {
+	timeout = Number.isFinite(timeout) && timeout > 0 ? Math.floor(timeout) : 0;
+	const keys = new Set();
 
-	function createPromise(value) {
+	let __promise;
+
+	function createPromise() {
 		return new Promise((resolve, reject) =>
 			setTimeout(() => {
-				const handlerKeys = handlers.get(value) || [];
-				handlers.delete(value);
-				promises.delete(value);
 				try {
-					resolve(value.apply(thisArg, handlerKeys));
+					const keysRequested = Array.from(keys);
+					keys.clear();
+					resolve(initiator.apply(thisArg, keysRequested));
 				} catch (error) {
 					reject(error);
 				}
 			}, timeout)
-		);
-	}
-
-	function markCall(key, value) {
-		const handlerKeys = handlers.get(value) || [];
-		handlerKeys.push(key);
-		handlers.set(value, handlerKeys);
+		)
+			.then((payload) => {
+				__promise = undefined;
+				return payload;
+			})
+			.catch((error) => {
+				__promise = undefined;
+				throw error;
+			});
 	}
 
 	function getPromise(key, value) {
 		if (typeof value === 'function') {
-			markCall(key, value);
-			let promise = promises.get(value);
-			if (!promise) {
-				promise = createPromise(value);
-				promises.set(value, promise);
-			}
-			return promise
-				.then((payload) => {
-					const afterHandler = after[key];
-					return typeof afterHandler === 'function' ?
-						afterHandler(payload) :
-						payload;
-				});
+			keys.add(key);
+			return (__promise ? __promise : __promise = createPromise()).then((payload) => value.call(thisArg, payload));
 		} else {
 			return Promise.resolve(value);
 		}
 	}
 
-	return new Proxy(source, {
+	return new Proxy(formatter, {
 		get(target, key) {
 			return getPromise(key, target[key]);
 		},
 		set(target, property, value) {
-			target[property] = value;
-			return value;
+			return target[property] = value;
 		}
 	});
 };
--- source/Standard/Object/async-get/test.js
+++ source/Standard/Object/async-get/test.js
@@ -2,184 +2,308 @@ const expect = require('chai').expect;
 const testee = require('./index');
 
 describe('async-get', () => {
+
 	describe('sync functions', () => {
+
 		it('normal', () => {
-			let i1 = 0;
+			let i = 0;
 
-			function f1() {
-				i1++;
-				return Array.prototype.slice.apply(arguments).join('');
+			function initiator() {
+				i++;
+				return Array.prototype.slice.apply(arguments)
+					.reduce((a, i) => {
+						a[i] = i;
+						return a;
+					}, {});
 			}
 
-			let i2 = 0;
-
-			function f2() {
-				i2++;
-				return Array.prototype.slice.apply(arguments).join('');
-			}
-
-			const source = {
-				a: f1,
-				b: f1,
-				c: f2
+			let formatter = {
+				a: ({a} = {}) => a + 9,
+				b: ({b} = {}) => b + 99,
+				c: ({c} = {}) => c + 999
 			};
 
-			let wrapped = testee(source, 10);
+			let wrapped = testee(initiator, formatter, 10);
 
 			return Promise.all([wrapped.a, wrapped.b, wrapped.c])
-				.then(([r1, r2, r3]) => {
-					expect(i1).to.equal(1);
-					expect(i2).to.equal(1);
-					expect(r1).to.equal('ab');
-					expect(r2).to.equal('ab');
-					expect(r3).to.equal('c');
+				.then(([a, b, c]) => {
+					expect(i).to.equal(1);
+					expect(a).to.equal('a9');
+					expect(b).to.equal('b99');
+					expect(c).to.equal('c999');
 				});
 		});
 
-		it('throw', () => {
-			let i1 = 0;
+		it('thisArg', () => {
+			let i = 0;
 
-			function f1() {
-				i1++;
-				return Array.prototype.slice.apply(arguments).join('');
+			const _this = {a: 2, b: 3};
+
+			function initiator() {
+				i++;
+				return this;
 			}
 
-			let i2 = 0;
-
-			function f2() {
-				i2++;
-				throw new Error(Array.prototype.slice.apply(arguments).join(''));
-			}
-
-			const source = {
-				a: f1,
-				b: f1,
-				c: f2
+			let formatter = {
+				a({a}) {
+					return this.a * a;
+				},
+				b({b}) {
+					return this.b * b;
+				}
 			};
 
-			let wrapped = testee(source, 10);
+			let wrapped = testee(initiator, formatter, 10, _this);
 
-			return Promise.all([wrapped.a, wrapped.b, wrapped.c.catch((e) => e)])
-				.then(([r1, r2, r3]) => {
-					expect(i1).to.equal(1);
-					expect(i2).to.equal(1);
-					expect(r1).to.equal('ab');
-					expect(r2).to.equal('ab');
-					expect(r3).to.be.an.instanceof(Error);
+			return Promise.all([wrapped.a, wrapped.b])
+				.then(([a, b, c]) => {
+					expect(i).to.equal(1);
+					expect(a).to.equal(4);
+					expect(b).to.equal(9);
 				});
 		});
 
-		it('set', () => {
+		it('default', () => {
+			let i = 0;
 
-			function f1() {
-				return {a: 3, b: 4};
-			}
-
-			function f2() {
-				return {c: 5};
-			}
-
-			const source = {
-				a: f1,
-				b: f1,
-				c: f2
+			const initiator = () => {
+				i++;
+				return ({a: 2, b: 3, c: 4});
 			};
 
-			let wrapped = testee(source, {a: ({a}) => {
-					console.log('DEBUG:test.js(a):89 =====================>');
-				return a
-			}}, 10);
+			let formatter = {
+				a: ({a} = {}) => a,
+				b: ({b} = {}) => b,
+				c: ({c} = {}) => c
+			};
 
-			return Promise.all([wrapped.a, wrapped.b, wrapped.c.catch((e) => e)])
-				.then(([r1, r2, r3]) => {
-					expect(r1).to.equal(3);
-					expect(r2).to.deep.equal({a: 3, b: 4});
-					expect(r3).to.deep.equal({c: 5});
+			let wrapped = testee(initiator, formatter, 10);
+
+			return Promise.all([wrapped.a, wrapped.b, wrapped.c, wrapped.d])
+				.then(([a, b, c, d]) => {
+					expect(i).to.equal(1);
+					expect(a).to.equal(2);
+					expect(b).to.equal(3);
+					expect(c).to.equal(4);
+					expect(d).to.equal(undefined);
 				});
 		});
+
+		it('throws. initiator', () => {
+			let i = 0;
+
+			const initiator = () => {
+				i++;
+				throw new Error('Oops');
+			};
+
+			let formatter = {
+				a: ({a} = {}) => a + 10,
+				b: ({b} = {}) => b + 100,
+				c: ({c} = {}) => c + 1000
+			};
+
+			let wrapped = testee(initiator, formatter, 10);
+
+			return Promise.all([wrapped.a, wrapped.b, wrapped.c])
+				.catch((error) => {
+					expect(i).to.equal(1);
+					expect(error).to.be.an.instanceof(Error);
+					expect(error.message).to.equal('Oops');
+				});
+		});
+
+		it('throws. formatter', () => {
+			let i = 0;
+
+			const initiator = () => {
+				i++;
+				return {a: 2, b: 3, c: 4};
+			};
+
+			let formatter = {
+				a: ({a} = {}) => a + 10,
+				b: ({b} = {}) => b + 100,
+				c: () => {
+					throw new Error('Oops');
+				}
+			};
+
+			let wrapped = testee(initiator, formatter, 10);
+
+			return Promise.all([wrapped.a, wrapped.b, wrapped.c.catch((e) => e)])
+				.then(([a, b, c]) => {
+					expect(i).to.equal(1);
+					expect(a).to.equal(12);
+					expect(b).to.equal(103);
+					expect(c).to.be.an.instanceof(Error);
+				});
+		});
+
+		it('bunched get requests', () => {
+			let i = 0;
+
+			const initiator = () => {
+				i++;
+				return {a: 2, b: 3, c: 4};
+			};
+
+			let formatter = {
+				a: ({a} = {}) => a,
+				b: ({b} = {}) => b,
+				c: ({c} = {}) => c
+			};
+
+			let wrapped = testee(initiator, formatter, 50);
+
+			return new Promise((resolve, reject) => {
+				Promise.all([wrapped.a, wrapped.b, wrapped.c]).then(() => expect(i).to.equal(1)).catch((e) => reject(e));
+
+				setTimeout(() => Promise.all([wrapped.a, wrapped.b, wrapped.c]).then(() => expect(i).to.equal(1)).catch((e) => reject(e)), 25);
+
+				setTimeout(() => Promise.all([wrapped.a, wrapped.b, wrapped.c]).then(() => expect(i).to.equal(2)).catch((e) => reject(e)), 75);
+
+				setTimeout(() => resolve(), 150);
+			});
+		});
+
 	});
 
 	describe('async functions', () => {
-		it('resolve', function () {
-			let i1 = 0;
 
-			function f1() {
+		it('initiator', () => {
+			let i = 0;
+
+			function initiator() {
+				i++;
 				return new Promise((resolve) => {
 					setTimeout(() => {
-						i1++;
-						resolve(Array.prototype.slice.apply(arguments).join(''));
+						resolve(Array.prototype.slice.apply(arguments)
+							.reduce((a, i) => {
+								a[i] = i;
+								return a;
+							}, {}));
 					}, 50);
 				});
 			}
 
-			let i2 = 0;
-
-			function f2() {
-				return new Promise((resolve) => {
-					setTimeout(() => {
-						i2++;
-						resolve(Array.prototype.slice.apply(arguments).join(''));
-					}, 50);
-				});
-			}
-
-			const source = {
-				a: f1,
-				b: f1,
-				c: f2
+			let formatter = {
+				a: ({a} = {}) => a + 9
 			};
 
-			let wrapped = testee(source, 10);
+			let wrapped = testee(initiator, formatter, 10);
+
+			return Promise.all([wrapped.a])
+				.then(([a]) => {
+					expect(i).to.equal(1);
+					expect(a).to.equal('a9');
+				});
+		});
+
+		it('default', () => {
+			let i = 0;
+
+			const initiator = () => {
+				i++;
+				return ({a: 2, b: 3, c: 4});
+			};
+
+			let formatter = {
+				a: ({a} = {}) => new Promise((resolve) => setTimeout(() => resolve(a), 50)),
+				b: ({b} = {}) => new Promise((resolve) => setTimeout(() => resolve(b), 50)),
+				c: ({c} = {}) => new Promise((resolve) => setTimeout(() => resolve(c), 50))
+			};
+
+			let wrapped = testee(initiator, formatter, 10);
 
 			return Promise.all([wrapped.a, wrapped.b, wrapped.c])
-				.then(([r1, r2, r3]) => {
-					expect(i1).to.equal(1);
-					expect(i2).to.equal(1);
-					expect(r1).to.equal('ab');
-					expect(r2).to.equal('ab');
-					expect(r3).to.equal('c');
+				.then(([a, b, c]) => {
+					expect(i).to.equal(1);
+					expect(a).to.equal(2);
+					expect(b).to.equal(3);
+					expect(c).to.equal(4);
 				});
 		});
 
-		it('reject', () => {
-			let i1 = 0;
+		it('throws. initiator', () => {
+			let i = 0;
 
-			function f1() {
-				return new Promise((resolve) => {
-					setTimeout(() => {
-						i1++;
-						resolve(Array.prototype.slice.apply(arguments).join(''));
-					}, 50);
-				});
+			function initiator() {
+				i++;
+				return new Promise((resolve, reject) => setTimeout(() => reject(new Error('Oops')), 50));
 			}
 
-			let i2 = 0;
-
-			function f2() {
-				return new Promise((resolve, reject) => {
-					setTimeout(() => {
-						i2++;
-						reject(new Error());
-					}, 50);
-				});
-			}
-
-			const source = {
-				a: f1,
-				b: f1,
-				c: f2
+			let formatter = {
+				a: ({a} = {}) => a + 10,
+				b: ({b} = {}) => b + 100,
+				c: ({c} = {}) => c + 1000
 			};
 
-			let wrapped = testee(source, 10);
+			let wrapped = testee(initiator, formatter, 10);
 
-			return Promise.all([wrapped.a, wrapped.b, wrapped.c.catch((e) => e)])
-				.then(([r1, r2, r3]) => {
-					expect(i1).to.equal(1);
-					expect(i2).to.equal(1);
-					expect(r1).to.equal('ab');
-					expect(r2).to.equal('ab');
-					expect(r3).to.be.an.instanceof(Error);
+			return Promise.all([wrapped.a, wrapped.b, wrapped.c])
+				.catch((error) => {
+					expect(i).to.equal(1);
+					expect(error).to.be.an.instanceof(Error);
+					expect(error.message).to.equal('Oops');
 				});
 		});
+
+		it('throws. formatter', () => {
+			let i = 0;
+
+			const initiator = () => {
+				i++;
+				return {a: 2, b: 3, c: 4};
+			};
+
+			let formatter = {
+				a: ({a} = {}) => new Promise((resolve) => setTimeout(() => resolve(a), 50)),
+				b: ({b} = {}) => new Promise((resolve) => setTimeout(() => resolve(b), 50)),
+				c: () => new Promise((resolve, reject) => setTimeout(() => reject(new Error('Oops')), 50))
+			};
+
+			let wrapped = testee(initiator, formatter, 10);
+
+			return Promise.all([wrapped.a, wrapped.b, wrapped.c.catch((e) => e)])
+				.then(([a, b, c]) => {
+					expect(i).to.equal(1);
+					expect(a).to.equal(2);
+					expect(b).to.equal(3);
+					expect(c).to.be.an.instanceof(Error);
+				});
+		});
+
+		it('bunched get requests', () => {
+			let i = 0;
+
+			function initiator() {
+				i++;
+				return new Promise((resolve) => setTimeout(() => resolve(), 25));
+			}
+
+			let formatter = {
+				a: ({a} = {}) => a,
+				b: ({b} = {}) => b,
+				c: ({c} = {}) => c
+			};
+
+			let wrapped = testee(initiator, formatter, 50);
+
+			return new Promise((resolve, reject) => {
+				Promise.all([wrapped.a, wrapped.b, wrapped.c]).then(() => expect(i).to.equal(1)).catch((e) => reject(e));
+
+				setTimeout(() => Promise.all([wrapped.a, wrapped.b, wrapped.c]).then(() => expect(i).to.equal(1)).catch((e) => reject(e)), 25);
+
+				setTimeout(() => Promise.all([wrapped.a, wrapped.b, wrapped.c]).then(() => expect(i).to.equal(1)).catch((e) => reject(e)), 50);
+
+				setTimeout(() => Promise.all([wrapped.a, wrapped.b, wrapped.c]).then(() => expect(i).to.equal(2)).catch((e) => reject(e)), 125);
+
+				setTimeout(() => resolve(), 200);
+			});
+
+		});
+
 	});
+
 });
