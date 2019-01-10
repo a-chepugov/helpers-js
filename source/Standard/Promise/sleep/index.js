ed72a5c95f60263d1a97786eb86934cd2d87194b
a.chepugov <oleksii.chepugov@ria.com>
2019-01-10 18:56:03 +0200

    chore(sleep)

--- source/Standard/Promise/sleep/index.js
+++ source/Standard/Promise/sleep/index.js
@@ -2,6 +2,6 @@
  * @name sleep
  * @memberof Standard/Promise
  * @param {Number} timeout
- * @return {Promise<any>}
+ * @return {Promise<Number>}
  */
 module.exports = (timeout) => new Promise((resolve) => setTimeout(resolve, timeout, timeout));
