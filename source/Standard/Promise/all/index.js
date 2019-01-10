a31f4d17969b6186894dd83e19d7bc98e20ea906
a.chepugov <oleksii.chepugov@ria.com>
2019-01-10 18:56:26 +0200

    feature(Promise.all)

---
+++ source/Standard/Promise/all/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * Promise.all variant with concurrency limitation
+ * @name all
+ * @memberof Standard/Promise
+ * @param {Array.Function} fnsList
+ * @param {Number} concurrency - number of concurrently running by Promise.all functions
+ * @return {Promise<Array.any>}
+ */
+module.exports = async function (fnsList, concurrency) {
+	if (!(Number.isInteger(concurrency) && concurrency > 0)) {
+		return Promise.reject(new Error(`Second argument must be a positive Integer. Got: ${concurrency}`));
+	}
+
+	if (!Array.isArray(fnsList)) {
+		return Promise.reject(new Error(`First argument must be an Array. Got: ${fnsList}`));
+	}
+	const list = fnsList.slice(0, fnsList.length);
+	let chunk;
+
+	const resultsAll = [];
+
+	while ((chunk = list.splice(0, concurrency)).length) {
+		await
+			Promise.all(
+				chunk
+					.map((item) =>
+						typeof item === 'function' ?
+							item() :
+							Promise.reject(new Error(`Item must be a function. Got: ${item}`))
+					))
+				.then((payload) => resultsAll.splice(resultsAll.length, 0, ...payload));
+	}
+
+	return resultsAll;
+}
