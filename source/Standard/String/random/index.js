1796c8c27aad70f286dc722a5f967de8e2d34f8a
a.chepugov <oleksii.chepugov@ria.com>
2019-01-15 13:00:11 +0200

    test(String) random

--- source/Standard/String/random/index.js
+++ source/Standard/String/random/index.js
@@ -1,23 +1,10 @@
-'use strict';
-
-const DEFAULT_LENGTH = 10;
-const DEFAULT_RADIX = 36;
 /**
  * Возвращает случайную строку заданной длины
  * @name random
  * @memberof Standard/String
- * @param {number} length - длинна строки
- * @param {number} radix - "разнообразие" символов
- * @return {string}
+ * @param {Number} [length=10] - длина строки
+ * @param {Number} [radix=36] - "разнообразие" символов
+ * @return {String}
  */
-module.exports = function (length, radix) {
-	length = Number.isInteger(length) && length >= 0 ?
-		length :
-		DEFAULT_LENGTH;
-
-	radix = Number.isInteger(radix) && radix >= 2 && radix <= DEFAULT_RADIX ?
-		radix :
-		DEFAULT_RADIX;
-
-	return Array.from(new Array(length), () => Math.floor(Math.random() * radix).toString(radix)).join('');
-};
+module.exports = (length = 10, radix = 36) =>
+	Array.from(new Array(length), () => Math.floor(Math.random() * radix).toString(radix)).join('');
