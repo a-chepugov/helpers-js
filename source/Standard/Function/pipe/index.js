b6ef4698bd95c4fd5784a7c9e1de232a77410541
a.chepugov <oleksii.chepugov@ria.com>
2019-01-15 15:18:35 +0200

    feature(Function) pipe

---
+++ source/Standard/Function/pipe/index.js
@@ -0,0 +1,10 @@
+/**
+ * Pipe functions
+ * @return {Function}
+ */
+module.exports = function () {
+	return Array.prototype.slice.call(arguments)
+		.reduce((a, fn) => function () {
+			return fn.call(this, a.apply(this, arguments));
+		}.bind(this));
+};
---
+++ source/Standard/Function/pipe/test.js
@@ -0,0 +1,36 @@
+const expect = require('chai').expect;
+
+const testee = require('./index');
+
+describe('pipe', () => {
+
+	describe('run', () => {
+
+		it('1', () => {
+			const fn1 = (a) => a * 2;
+			const fn2 = (a) => a + 1;
+			const fn3 = (a) => a ** 2;
+
+			const wrapped = testee(fn1, fn2, fn3);
+			expect(wrapped(1)).to.equal(9);
+		});
+
+		it('this', () => {
+			const _this = {a: 1};
+			const fn1 = function () {
+				expect(this).to.deep.equal(_this);
+			};
+			const fn2 = function () {
+				expect(this).to.deep.equal(_this);
+			};
+			const fn3 = function () {
+				expect(this).to.deep.equal(_this);
+			};
+
+			const wrapped = testee.call(_this, fn1, fn2, fn3);
+			wrapped();
+		});
+
+	});
+
+});
