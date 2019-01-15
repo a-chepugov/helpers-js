4f48a28337b82f707c471708a2a6fc9fc6fa1ccd
a.chepugov <oleksii.chepugov@ria.com>
2019-01-15 12:03:44 +0200

    test(Number) loopify

--- source/Standard/Number/loopify/index.js
+++ source/Standard/Number/loopify/index.js
@@ -1,11 +1,9 @@
-'use strict';
-
 /**
  * Возвращает позицию для элемента закольцованного массива (элементы 1 и N + 1 идентичны)
  * @module loopify
  * @memberof Standard/Number
- * @param {number} i - позиция элемента
- * @param {number} N - количество элементов в массиве
- * @return {number} - откорректированная позиция в массиве
+ * @param {Number} i - позиция элемента
+ * @param {Number} N - количество элементов в массиве
+ * @return {Number} - откорректированная позиция в массиве
  */
-module.exports = (i, N) => ((i % N) + N) % N
+module.exports = (i, N) => ((i % N) + N) % N;
---
+++ source/Standard/Number/loopify/test.js
@@ -0,0 +1,29 @@
+const expect = require('chai').expect;
+const testee = require('./index');
+
+describe('loopify', () => {
+
+	describe('run', () => {
+
+		it('in range',
+			() => expect((testee(1, 10))).to.equal(1));
+
+		it('out of range. positive',
+			() => expect(testee(22, 20)).to.equal(2));
+
+		it('out of range. negative',
+			() => expect(testee(-1, 20)).to.equal(19));
+
+	});
+
+	describe('invalid', () => {
+
+		it('position',
+			() => expect(testee(1, NaN)).to.deep.equal(NaN));
+
+		it('max index',
+			() => expect(testee(NaN, 1)).to.deep.equal(NaN));
+
+	});
+
+});
