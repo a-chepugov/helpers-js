80e45c76542c2f9aaa0732317a9bae56f5a00bb5
a.chepugov <oleksii.chepugov@ria.com>
2020-03-06 17:23:29 +0200

    chore(Generator) js -> ts

---
+++ source/Standard/Generator/Navigable/index.test.ts
@@ -0,0 +1,56 @@
+const expect = require('chai').expect;
+
+const Testee = require('./index').default;
+
+describe('Navigable', () => {
+
+    const fn = (position: number) => ({value: position, done: !(position < 10)});
+
+    it('is iterable', () => {
+        const instance = new Testee(fn);
+        let index = 0;
+        for (let item of instance) {
+            expect(item).to.be.equal(index++);
+        }
+    });
+
+    it('position', () => {
+        const instance = new Testee(fn);
+        instance.position = 5;
+        expect(instance.position).to.be.equal(5);
+    });
+
+    it('current', () => {
+        const instance = new Testee(fn);
+        instance.position = 5;
+        expect(instance.current().value).to.be.equal(5);
+    });
+
+    it('next', () => {
+        const instance = new Testee(fn);
+        instance.position = 5;
+        expect(instance.next().value).to.be.equal(6);
+    });
+
+    it('back', () => {
+        const instance = new Testee(fn);
+        instance.position = 5;
+        expect(instance.back().value).to.be.equal(4);
+    });
+
+    it('skip', () => {
+        const instance = new Testee(fn);
+        instance.skip(5);
+        expect(instance.position).to.be.equal(5);
+    });
+
+    it('take', () => {
+        const instance = new Testee(fn);
+        expect(instance.take(5).value).to.be.equal(5);
+        expect(instance.take(5).done).to.be.equal(false);
+
+        expect(instance.take(10).value).to.be.equal(10);
+        expect(instance.take(10).done).to.be.equal(true);
+    });
+
+});
---
+++ source/Standard/Generator/Navigable/index.ts
@@ -0,0 +1,53 @@
+export interface Fn {
+    (position: number): { value: any, done: Boolean }
+}
+
+
+export default class Navigable {
+    private fn: Fn;
+
+    // @ts-ignore
+    private _position: number;
+
+    constructor(fn: Fn = (position: number) => ({value: position, done: false})) {
+        this.fn = fn;
+    }
+
+    get position() {
+        return this._position;
+    }
+
+    set position(value: number) {
+        if (Number.isInteger(value)) {
+            this._position = value;
+        } else {
+            throw new Error('Must be a number. Got: ' + value);
+        }
+    }
+
+    back() {
+        this.position = Number.isInteger(this.position) ? this.position - 1 : 0;
+        return this.take(this.position);
+    }
+
+    current() {
+        return this.take(this.position);
+    }
+
+    next() {
+        this.position = Number.isInteger(this.position) ? this.position + 1 : 0;
+        return this.take(this.position);
+    }
+
+    skip(value: number) {
+        return this.position = Number.isInteger(this.position) ? this.position + value : value;
+    }
+
+    take(position: number) {
+        return this.fn.call(this, position);
+    }
+
+    [Symbol.iterator]() {
+        return this;
+    }
+};
