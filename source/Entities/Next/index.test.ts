49315458a170b3795e7d034772f39e1e5694ac04
Алексей Чепугов <a.chepugov@gmail.com>
2020-07-08 16:28:37 +0300

    chore (Next) remove nested function

--- source/Entities/Next/index.test.ts
+++ source/Entities/Next/index.test.ts
@@ -4,61 +4,60 @@ import Next from './index';
 describe('Next', () => {
 
 	it('can be constructed from function', () => {
-		return new Next((cb: any) => (...args: number[]) => cb(...args))
-			.next((cb: any) => (...args: number[]) => cb(...args.map((i: number) => i ** 2)))
+		return new Next((cb: any, ...args: number[]) => cb(...args))
+			.next((cb: any, ...args: number[]) => cb(...args.map((i: number) => i ** 2)))
 			.execute((...args: number[]) => {
 				return expect(args).to.be.deep.equal([1, 4, 9])
-			})(1, 2, 3);
+			}, 1, 2, 3);
 	});
 
 	it('can be constructed from primitives', () => {
 		return Next.of(1, 2, 3)
-			.next((cb: any) => (...args: number[]) => cb(...args.map((i: any) => i ** 2)))
+			.next((cb: any, ...args: number[]) => cb(...args.map((i: any) => i ** 2)))
 			.execute((...args: number[]) => {
 				return expect(args).to.be.deep.equal([1, 4, 9])
-			})();
+			});
 	});
 
 	it('can run synchronously', () => {
 		const result = Next.of(1, 2, 3)
-			.next((cb: any) => (...args: number[]) => cb(...args.map((i: number) => i ** 2)))
-			.execute((...args: number[]) => args)();
+			.next((cb: any, ...args: number[]) => cb(...args.map((i: number) => i ** 2)))
+			.execute((...args: number[]) => args);
 
 		expect(result).to.be.deep.equal([1, 4, 9])
 	});
 
 	it('can run asynchronously', (done) => {
 		const result = Next.of(1, 2, 3)
-			.next((cb: any) => (...args: number[]) => setTimeout(() => cb(...args.map((i: any) => i ** 2))))
+			.next((cb: any, ...args: number[]) => setTimeout(() => cb(...args.map((i: any) => i ** 2))))
 			.execute((...args: number[]) => {
 				expect(args).to.be.deep.equal([1, 4, 9])
 				expect(result).to.not.be.deep.equal(args)
 				done();
-			})();
+			});
 	});
 
 	it('stays lazy until executed', () => {
 		let counter = 0;
 
 		const next = Next.of()
-			.next((cb: any) => () => cb(counter++))
-			.next((cb: any) => () => cb(counter++))
-			.next((cb: any) => () => cb(counter++))
+			.next((cb: any) => cb(counter++))
+			.next((cb: any) => cb(counter++))
+			.next((cb: any) => cb(counter++))
 
 		expect(counter).to.be.equal(0)
 
-		next.execute((...args: number[]) => args)();
+		next.execute((...args: number[]) => args);
 		expect(counter).to.be.equal(3)
 	});
 
-	it('can pass context', () => {
-		const context = {a: 1, b: 2, c: 3};
-
-		return new Next((cb: any) =>
-			function (this: any) {
-				return expect(this).to.be.deep.equal(context)
+	it('unhandled error emerges after execume method invokes', () => {
+		const next = Next.of()
+			.next((_cb: any) => {
+				throw new Error('Unhandled error');
 			})
-			.execute((...args: any[]) => args).call(context);
+
+		expect(() => next.execute(() => undefined)).to.throw();
 	});
 
 });
--- source/Entities/Next/index.ts
+++ source/Entities/Next/index.ts
@@ -1,8 +1,7 @@
 type CB<ARGS extends any[], RESULT> = (...args: ARGS) => RESULT;
 
 type Action<CB_ARGS extends any[], CB_RESULT, A_ARGS extends any[], A_RESULT> =
-	(callback: CB<CB_ARGS, CB_RESULT>) =>
-		(...args: A_ARGS) => A_RESULT;
+	(callback: CB<CB_ARGS, CB_RESULT>, ...args: A_ARGS) => A_RESULT;
 
 export default class Next<CB_ARGS extends any[], CB_RESULT, A_ARGS extends any[], A_RESULT> {
 	action: Action<CB_ARGS, CB_RESULT, A_ARGS, A_RESULT>;
@@ -12,21 +11,18 @@ export default class Next<CB_ARGS extends any[], CB_RESULT, A_ARGS extends any[]
 	}
 
 	static of(...args: any[]) {
-		return new Next((callback: CB<typeof args, any>) => () => callback(...args));
+		return new Next((callback: CB<typeof args, any>) => callback(...args));
 	}
 
-	execute(callback?: CB<CB_ARGS, CB_RESULT>) {
-		const action = this.action;
-		return function (this: any, ...args: A_ARGS) {
-			return action(callback).apply(this, args);
-		}
+	execute(callback: CB<CB_ARGS, CB_RESULT>, ...args: A_ARGS) {
+		return this.action(callback, ...args);
 	}
 
-	next<CB_ARGS_2 extends any[]>(layer: (callback: CB<CB_ARGS_2, CB_RESULT>) => (...args: CB_ARGS) => any) {
-		return new Next((callback2: CB<CB_ARGS_2, CB_RESULT>) => (...args0: A_ARGS) => {
+	next<CB_ARGS_2 extends any[]>(layer: (callback: CB<CB_ARGS_2, CB_RESULT>, ...args: CB_ARGS) => any) {
+		return new Next((callback2: CB<CB_ARGS_2, CB_RESULT>, ...args0: A_ARGS) => {
 			return this.execute((...args1: CB_ARGS) => {
-				return layer(callback2)(...args1);
-			})(...args0)
+				return layer(callback2, ...args1);
+			}, ...args0)
 		});
 	}
 
