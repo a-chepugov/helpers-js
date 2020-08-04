0231657a46fcb9c50513fc706f9b3bdd84fbd808
Алексей Чепугов <a.chepugov@gmail.com>
2020-08-04 14:33:40 +0300

    feature (Next) remove unnecessary layers in `execute` method

--- source/Entities/Next/index.ts
+++ source/Entities/Next/index.ts
@@ -4,23 +4,20 @@ type Action<CB_ARGS extends any[], CB_RESULT, A_ARGS extends any[], A_RESULT> =
 	(callback: CB<CB_ARGS, CB_RESULT>, ...args: A_ARGS) => A_RESULT;
 
 export default class Next<CB_ARGS extends any[], CB_RESULT, A_ARGS extends any[], A_RESULT> {
-	action: Action<CB_ARGS, CB_RESULT, A_ARGS, A_RESULT>;
+	readonly execute: Action<CB_ARGS, CB_RESULT, A_ARGS, A_RESULT>;
 
 	constructor(action: Action<CB_ARGS, CB_RESULT, A_ARGS, A_RESULT>) {
-		this.action = action;
+		Object.defineProperty(this, 'execute', {value: action});
 	}
 
 	static of(...args: any[]) {
 		return new Next((callback: CB<typeof args, any>) => callback(...args));
 	}
 
-	execute = (callback: CB<CB_ARGS, CB_RESULT>, ...args: A_ARGS) => {
-		return this.action(callback, ...args);
-	}
-
 	next<CB_ARGS_2 extends any[]>(layer: (callback: CB<CB_ARGS_2, CB_RESULT>, ...args: CB_ARGS) => any) {
+		const execute = this.execute;
 		return new Next((callback2: CB<CB_ARGS_2, CB_RESULT>, ...args1: A_ARGS) => {
-			return this.action((...args2: CB_ARGS) => {
+			return execute((...args2: CB_ARGS) => {
 				return layer(callback2, ...args2);
 			}, ...args1)
 		});
